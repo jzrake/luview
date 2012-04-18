5e02044be5c621c4b4d91f9c8d7f2fe93e5dccc2
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-04-18 17:52:33 -0400

    Fresh start under the old build system.

--- src/image.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef __Image_HEADER__
-#define __Image_HEADER__
-
-void luview_write_ppm(const char *fname, const double *data, int cmap,
-		      int Nx, int Ny, const double *range);
-const float *luview_get_colormap(int cmap);
-
-#endif // __Image_HEADER__
