d64c4f69914c3cb7601f64cd7b62358f47cb816c
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-04-20 17:37:17 -0400

    Clean up

--- test/load.lua
+++ test/load.lua
@@ -6,10 +6,7 @@ local luview = require 'luview'
 local window = luview.Window()
 local bounding_box = luview.BoundingBox()
 local little_box = luview.BoundingBox()
---local nurbs = luview.ExampleSimpleNURBS()
-local nurbs = luview.SurfaceNURBS()
-local evaluator = luview.ExampleSimpleEvaluator()
-local surface = luview.SurfaceEvaluator()
+local surface = luview.SurfaceNURBS()
 
 
 local function MakeSurfaceData()
@@ -25,16 +22,8 @@ local function MakeSurfaceData()
 end
 
 
-nurbs:set_data(MakeSurfaceData())
-nurbs:set_position(0,-0.5,0)
-
---surface:set_data(surfdata)
-surface:set_position(0,-1,0)
-
-evaluator:set_color(1.0, 0.5, 0.0)
-evaluator:set_linewidth(1.0)
-
-
+surface:set_data(MakeSurfaceData())
+surface:set_position(0,-0.6,0)
 
 bounding_box:set_linewidth(2.5)
 bounding_box:set_color(0.1, 0.1, 0.6)
@@ -43,8 +32,7 @@ little_box:set_scale(0.2, 0.5, 0.5)
 little_box:set_orientation(0.0, 20.0, 0.0)
 little_box:set_color(1.0, 0.2, 0.1)
 
-
 window:set_orientation(45,0,0)
 window:set_position(0.0, 0.0, -2)
 window:set_color(0.7, 0.95, 0.95)
-window:render_scene({bounding_box, little_box, nurbs})
+window:render_scene({bounding_box, little_box, surface})
