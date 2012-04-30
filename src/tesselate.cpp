df594fc55ad87a46740903f177cf0ae114d3c3c8
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-04-30 10:53:56 -0400

    Trying to get voronoi diagram to look right

--- src/tesselate.cpp
+++ src/tesselate.cpp
@@ -35,19 +35,125 @@
 
 Tesselation3D::Tesselation3D()
 {
-  gl_modes.push_back(GL_LIGHTING);
-  gl_modes.push_back(GL_LIGHT0);
+  //  gl_modes.push_back(GL_LIGHTING);
+  //  gl_modes.push_back(GL_LIGHT0);
+  gl_modes.push_back(GL_NORMALIZE);
   gl_modes.push_back(GL_BLEND);
   gl_modes.push_back(GL_COLOR_MATERIAL);
-  gl_modes.push_back(GL_NORMALIZE);
 
-  Np = 360;
+  Np = 20;
   pointlist = new double[Np * 3];
-  for (int n=0; n<Np; ++n) {
+
+  int n;
+  double phi = (1.0 + sqrt(5.0)) / 2.0;
+
+  n=0;
+  pointlist[3*n + 0] =-1.0;
+  pointlist[3*n + 1] =-1.0;
+  pointlist[3*n + 2] =-1.0;
+
+  n=1;
+  pointlist[3*n + 0] =-1.0;
+  pointlist[3*n + 1] =-1.0;
+  pointlist[3*n + 2] =+1.0;
+
+  n=2;
+  pointlist[3*n + 0] =-1.0;
+  pointlist[3*n + 1] =+1.0;
+  pointlist[3*n + 2] =-1.0;
+
+  n=3;
+  pointlist[3*n + 0] =-1.0;
+  pointlist[3*n + 1] =+1.0;
+  pointlist[3*n + 2] =+1.0;
+
+  n=4;
+  pointlist[3*n + 0] =+1.0;
+  pointlist[3*n + 1] =-1.0;
+  pointlist[3*n + 2] =-1.0;
+
+  n=5;
+  pointlist[3*n + 0] =+1.0;
+  pointlist[3*n + 1] =-1.0;
+  pointlist[3*n + 2] =+1.0;
+
+  n=6;
+  pointlist[3*n + 0] =+1.0;
+  pointlist[3*n + 1] =+1.0;
+  pointlist[3*n + 2] =-1.0;
+
+  n=7;
+  pointlist[3*n + 0] =+1.0;
+  pointlist[3*n + 1] =+1.0;
+  pointlist[3*n + 2] =+1.0;
+
+  n=8;
+  pointlist[3*n + 0] = 0.0;
+  pointlist[3*n + 1] =-1.0/phi;
+  pointlist[3*n + 2] =-phi;
+
+  n=9;
+  pointlist[3*n + 0] = 0.0;
+  pointlist[3*n + 1] =-1.0/phi;
+  pointlist[3*n + 2] =+phi;
+
+  n=10;
+  pointlist[3*n + 0] = 0.0;
+  pointlist[3*n + 1] =+1.0/phi;
+  pointlist[3*n + 2] =-phi;
+
+  n=11;
+  pointlist[3*n + 0] = 0.0;
+  pointlist[3*n + 1] =+1.0/phi;
+  pointlist[3*n + 2] =+phi;
+
+  n=12;
+  pointlist[3*n + 2] = 0.0;
+  pointlist[3*n + 0] =-1.0/phi;
+  pointlist[3*n + 1] =-phi;
+
+  n=13;
+  pointlist[3*n + 2] = 0.0;
+  pointlist[3*n + 0] =-1.0/phi;
+  pointlist[3*n + 1] =+phi;
+
+  n=14;
+  pointlist[3*n + 2] = 0.0;
+  pointlist[3*n + 0] =+1.0/phi;
+  pointlist[3*n + 1] =-phi;
+
+  n=15;
+  pointlist[3*n + 2] = 0.0;
+  pointlist[3*n + 0] =+1.0/phi;
+  pointlist[3*n + 1] =+phi;
+
+  n=16;
+  pointlist[3*n + 1] = 0.0;
+  pointlist[3*n + 2] =-1.0/phi;
+  pointlist[3*n + 0] =-phi;
+
+  n=17;
+  pointlist[3*n + 1] = 0.0;
+  pointlist[3*n + 2] =-1.0/phi;
+  pointlist[3*n + 0] =+phi;
+
+  n=18;
+  pointlist[3*n + 1] = 0.0;
+  pointlist[3*n + 2] =+1.0/phi;
+  pointlist[3*n + 0] =-phi;
+
+  n=19;
+  pointlist[3*n + 1] = 0.0;
+  pointlist[3*n + 2] =+1.0/phi;
+  pointlist[3*n + 0] =+phi;
+
+  /*
+    for (int n=0; n<Np; ++n) {
     pointlist[3*n + 0] = 0.5 - 1.0*rand() / RAND_MAX;
     pointlist[3*n + 1] = 0.5 - 1.0*rand() / RAND_MAX;
     pointlist[3*n + 2] = 0.5 - 1.0*rand() / RAND_MAX;
-  }
+    }
+  */
 }
 Tesselation3D::~Tesselation3D()
 {
@@ -56,6 +162,7 @@ Tesselation3D::~Tesselation3D()
 
 void Tesselation3D::draw_local()
 {
+  int delaunay = 1;
   tetgenio inp, out;
 
   inp.initialize();
@@ -78,35 +185,83 @@ void Tesselation3D::draw_local()
   glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
   glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
 
-  glBegin(GL_TRIANGLES);
-  for (int n=0; n<out.numberoftrifaces; ++n) {
-    int n0 = out.trifacelist[3*n + 0];
-    int n1 = out.trifacelist[3*n + 1];
-    int n2 = out.trifacelist[3*n + 2];
-    REAL *u = &out.pointlist[3*n0];
-    REAL *v = &out.pointlist[3*n1];
-    REAL *w = &out.pointlist[3*n2];
-    REAL normal[3];
-    compute_normal(u, v, w, normal);
-    glNormal3dv(normal);
-    glVertex3dv(u);
-    glVertex3dv(v);
-    glVertex3dv(w);
-  }
-  glEnd();
+  if (delaunay) {
+    glBegin(GL_TRIANGLES);
+    for (int n=0; n<out.numberoftrifaces; ++n) {
+      int n0 = out.trifacelist[3*n + 0];
+      int n1 = out.trifacelist[3*n + 1];
+      int n2 = out.trifacelist[3*n + 2];
+      REAL *u = &out.pointlist[3*n0];
+      REAL *v = &out.pointlist[3*n1];
+      REAL *w = &out.pointlist[3*n2];
+      REAL normal[3];
+      compute_normal(u, v, w, normal);
+      glNormal3dv(normal);
+      glVertex3dv(u);
+      glVertex3dv(v);
+      glVertex3dv(w);
+    }
+    glEnd();
 
-  glBegin(GL_LINES);
-  for (int n=0; n<out.numberofedges; ++n) {
-    int n0 = out.edgelist[2*n + 0];
-    int n1 = out.edgelist[2*n + 1];
-    REAL *u = &out.pointlist[3*n0];
-    REAL *v = &out.pointlist[3*n1];
-    REAL normal[3] = {u[0]-v[0], u[1]-v[1], u[2]-v[2]};
-    glNormal3dv(normal);
-    glVertex3dv(u);
-    glVertex3dv(v);
+    glBegin(GL_LINES);
+    for (int n=0; n<out.numberofedges; ++n) {
+      int n0 = out.edgelist[2*n + 0];
+      int n1 = out.edgelist[2*n + 1];
+      REAL *u = &out.pointlist[3*n0];
+      REAL *v = &out.pointlist[3*n1];
+      REAL normal[3] = {u[0]-v[0], u[1]-v[1], u[2]-v[2]};
+      glNormal3dv(normal);
+      glVertex3dv(u);
+      glVertex3dv(v);
+    }
+    glEnd();
   }
-  glEnd();
+
+  else {
+    /*
+    for (int n=0; n<out.numberofvfacets; ++n) {
+      tetgenio::vorofacet &f = out.vfacetlist[n];
+      std::vector<REAL*> verts;
+
+      for (int m=0; m<f.elist[0]; ++m) {
+        int eindex = f.elist[m+1];
+        int vindex = out.vedgelist[eindex].v1;
+        if (out.vedgelist[eindex].v2 == -1) {
+          printf("skipping ray\n");
+          continue;
+        }
+        verts.push_back(&out.vpointlist[3*vindex]);
+      }
+      if (verts.size() < 3) {
+        //      printf("skipping facet with %ld vertices\n", verts.size());
+        continue;
+      }
+
+      REAL normal[3];
+      compute_normal(verts[0], verts[1], verts[2], normal);
+      glNormal3dv(normal);
+
+      glBegin(GL_POLYGON);
+      for (unsigned int m=0; m<verts.size(); ++m) {
+        glVertex3dv(verts[m]);
+      }
+      glEnd();
+    }
+    */
+
+    glBegin(GL_LINES);
+    for (int n=0; n<out.numberofvedges; ++n) {
+      tetgenio::voroedge &e = out.vedgelist[n];
+      if (e.v2 == -1) continue; // -1 indicates ray
+      REAL *u = &out.vpointlist[3*e.v1];
+      REAL *v = &out.vpointlist[3*e.v2];
+      REAL normal[3] = {u[0]-v[0], u[1]-v[1], u[2]-v[2]};
+      glNormal3dv(normal);
+      glVertex3dv(u);
+      glVertex3dv(v);
+    }
+    glEnd();
+  }
 }
 
 void Tesselation3D::compute_normal(double *u, double *v, double *w, double *n)
@@ -117,6 +272,11 @@ void Tesselation3D::compute_normal(double *u, double *v, double *w, double *n)
   n[0] = uv[1]*uw[2] - uv[2]*uw[1];
   n[1] = uv[2]*uw[0] - uv[0]*uw[2];
   n[2] = uv[0]*uw[1] - uv[1]*uw[0];
+
+  double mag = sqrt(n[0]*n[0] + n[1]*n[1] + n[2]*n[2]);
+  n[0] /= mag;
+  n[1] /= mag;
+  n[2] /= mag;
 }
 
 
@@ -125,7 +285,7 @@ void Tesselation3D::compute_normal(double *u, double *v, double *w, double *n)
 
 
 
-  /*
+/*
   int n;
 
   n=0;
@@ -157,21 +317,64 @@ void Tesselation3D::compute_normal(double *u, double *v, double *w, double *n)
   pointlist[3*n + 0] = +0.5;
   pointlist[3*n + 1] = +0.5;
   pointlist[3*n + 2] =  0.0;
-  */
+*/
 
 
 
-  /*
-    glBegin(GL_LINES);
-    for (int n=0; n<out.numberofvedges; ++n) {
-    tetgenio::voroedge e = out.vedgelist[n];
-    if (e.v2 == -1) continue; // -1 indicates ray
-    int n0 = e.v1;
-    int n1 = e.v2;
-    REAL *u = &out.vpointlist[3*n0];
-    REAL *v = &out.vpointlist[3*n1];
-    glVertex3dv(u);
-    glVertex3dv(v);
-    }
-    glEnd();
-  */
+/*
+  glBegin(GL_LINES);
+  for (int n=0; n<out.numberofvedges; ++n) {
+  tetgenio::voroedge e = out.vedgelist[n];
+  if (e.v2 == -1) continue; // -1 indicates ray
+  int n0 = e.v1;
+  int n1 = e.v2;
+  REAL *u = &out.vpointlist[3*n0];
+  REAL *v = &out.vpointlist[3*n1];
+  glVertex3dv(u);
+  glVertex3dv(v);
+  }
+  glEnd();
+*/
+
+
+/*
+  n=0;
+  pointlist[3*n + 0] =-0.5;
+  pointlist[3*n + 1] =-0.5;
+  pointlist[3*n + 2] =-0.5;
+
+  n=1;
+  pointlist[3*n + 0] =-0.5;
+  pointlist[3*n + 1] =-0.5;
+  pointlist[3*n + 2] =+0.5;
+
+  n=2;
+  pointlist[3*n + 0] =-0.5;
+  pointlist[3*n + 1] =+0.5;
+  pointlist[3*n + 2] =-0.5;
+
+  n=3;
+  pointlist[3*n + 0] =-0.5;
+  pointlist[3*n + 1] =+0.5;
+  pointlist[3*n + 2] =+0.5;
+
+  n=4;
+  pointlist[3*n + 0] =+0.5;
+  pointlist[3*n + 1] =-0.5;
+  pointlist[3*n + 2] =-0.5;
+
+  n=5;
+  pointlist[3*n + 0] =+0.5;
+  pointlist[3*n + 1] =-0.5;
+  pointlist[3*n + 2] =+0.5;
+
+  n=6;
+  pointlist[3*n + 0] =+0.5;
+  pointlist[3*n + 1] =+0.5;
+  pointlist[3*n + 2] =-0.5;
+
+  n=7;
+  pointlist[3*n + 0] =+0.5;
+  pointlist[3*n + 1] =+0.5;
+  pointlist[3*n + 2] =+0.5;
+*/
