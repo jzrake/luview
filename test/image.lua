ad29213568e8735c81b9d173f10c56e771883eef
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-05-11 16:34:31 -0400

    Image plane works great here

--- test/image.lua
+++ test/image.lua
@@ -11,14 +11,24 @@ local image_src = luview.MultiImageSource()
 local image = luview.ImagePlane()
 local normalize = luview.GlobalLinearTransformation()
 local cmap = luview.ColorMaps()
-local rho = lunum.fromfile("/Users/jzrake/Work/luview/rho.bin"):reshape{1024,1024}
 
-image_src:set_array(rho['::2,::2'])
+local rhoJ = lunum.fromfile("/Users/jzrake/Work/luview/data/rhoJ.bin"):reshape{1024,1024}
+local rhoZ = lunum.fromfile("/Users/jzrake/Work/luview/data/rhoZ.bin"):reshape{1024,1024}
+local rho = lunum.zeros{1024,1024}
+
+for m=0,1024*1024-1 do
+   local j = m / 1024
+   rho[m] = j < 512 and rhoJ[m] or rhoZ[m]
+end
+
+
+image_src:set_array(rho['::1,::1'])
 normalize:set_input(image_src)
 normalize:set_range(0, 0.0, 1.0)
 scolors:set_input(normalize)
 scolors:set_transform(cmap)
 image:set_data("rgba", scolors)
+image:set_orientation(0,0,-90)
 window:set_color(0.2, 0.2, 0.2)
 window:set_orientation(0,0,0)
 window:set_position(0,0,-1.4)
