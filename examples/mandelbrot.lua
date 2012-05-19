ba0355be2244bd76012f09749e735344e74a9786
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-05-19 19:19:19 -0400

    This is a nice configuration for mandelbrot.

--- examples/mandelbrot.lua
+++ examples/mandelbrot.lua
@@ -27,46 +27,44 @@ void main() {
   vec2 c = %f*(pos.xy) - vec2(%f, %f);
   int n = 0;
 
-  while (z0.x*z0.x + z0.y*z0.y < 4.0 && n < 250) {
+  while (z0.x*z0.x + z0.y*z0.y < 4.0 && n < 500) {
      z1.x = z0.x*z0.x - z0.y*z0.y + c.x;
      z1.y = 2.0*z0.x*z0.y + c.y;
      z0 = z1;
      ++n;
   }
-  
-  gl_FragColor = vec4((-cos(0.02*float(n))+1.0)/2.0,
-                      (-sin(0.24*float(n))+1.0)/2.0,
-                      (-cos(0.12*float(n))+1.0)/2.0,
+
+  gl_FragColor = vec4((1.0-cos(0.02*float(n)))/2.0,
+                      (1.0-sin(0.12*float(n)))/2.0,
+                      (1.0-cos(0.18*float(n)))/2.0,
 		      1.0);
 }
 ]], zoom, x0, y0)
 end
 
-shade:set_program(vert, frag(3.0, 0.1, 0.0))
-
-local coords = lunum.zeros{512,512,4}
+local coords = lunum.zeros{8,8,4}
 scolors:set_array(coords)
 
 image:set_data("rgba", scolors)
 image:set_orientation(0,0,-90)
 image:set_shader(shade)
-window:set_color(0.2, 0.2, 0.2)
+window:set_color(0,0,0)
 window:set_orientation(0,0,0)
 window:set_position(0,0,-1.4)
 
-local status = "continue"
-local key
-local iter = 0
 
+local status = "continue"
+local iter = 0
+local key
 local params = { z=3, x=0, y=0 }
 
 while status == "continue" do
    status, key = window:render_scene{image}
 
-   if key == '[' then params.x = params.x + 0.01*params.z end
-   if key == ']' then params.x = params.x - 0.01*params.z end
-   if key == 'm' then params.y = params.y + 0.01*params.z end
-   if key == 'k' then params.y = params.y - 0.01*params.z end
+   if key == '[' then params.x = params.x + 0.03*params.z end
+   if key == ']' then params.x = params.x - 0.03*params.z end
+   if key == 'm' then params.y = params.y + 0.03*params.z end
+   if key == 'k' then params.y = params.y - 0.03*params.z end
    if key == '-' then params.z = params.z * 1.1 end
    if key == '+' then params.z = params.z / 1.1 end
 
