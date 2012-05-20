0ec9ebb4a95fe86bb1d2af76f2b2d06daff4a6ea
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-05-20 13:05:38 -0400

    Nice colors here on mandelbrot.

--- examples/mandelbrot.lua
+++ examples/mandelbrot.lua
@@ -20,6 +20,7 @@ void main() {
 
 local function frag(params)
    local zoom, x0, y0 = params.z, params.x, params.y
+   local rf, gf, bf = params.r, params.g, params.b
    return string.format([[
 varying vec4 pos;
 void main() {
@@ -35,12 +36,12 @@ void main() {
      ++n;
   }
 
-  gl_FragColor = vec4((1.0-cos(0.02*float(n)))/2.0,
-                      (1.0-sin(0.12*float(n)))/2.0,
-                      (1.0-cos(0.18*float(n)))/2.0,
+  gl_FragColor = vec4((1.0-cos(%f*float(n)))/2.0,
+                      (1.0-cos(%f*float(n)))/2.0,
+                      (1.0-cos(%f*float(n)))/2.0,
 		      1.0);
 }
-]], zoom, x0, y0)
+]], zoom, x0, y0, rf, gf, bf)
 end
 
 local coords = lunum.zeros{8,8,4}
@@ -54,17 +55,23 @@ window:set_orientation(0,0,0)
 window:set_position(0,0,-0.9)
 
 
-local params = { z=3, x=0, y=0 }
-shade:set_program(vert, frag(params))
+local p = { z=3, x=0, y=0, r=0.1, g=0.08, b=0.10 }
+shade:set_program(vert, frag(p))
 
-window:set_callback("a", function() params.x = params.x + 0.03*params.z end, "move left")
-window:set_callback("d", function() params.x = params.x - 0.03*params.z end, "move right")
-window:set_callback("s", function() params.y = params.y + 0.03*params.z end, "move down")
-window:set_callback("w", function() params.y = params.y - 0.03*params.z end, "move up")
-window:set_callback("-", function() params.z = params.z * 1.1 end, "zoom out")
-window:set_callback("+", function() params.z = params.z / 1.1 end, "zoom in")
-window:set_callback("idle", function() shade:set_program(vert, frag(params)) end)
+window:set_callback("P", function() for k,v in pairs(p) do print(k,v) end end,
+		    "print color params")
+window:set_callback("a", function() p.x = p.x + 0.03*p.z end, "move left")
+window:set_callback("d", function() p.x = p.x - 0.03*p.z end, "move right")
+window:set_callback("s", function() p.y = p.y + 0.03*p.z end, "move down")
+window:set_callback("w", function() p.y = p.y - 0.03*p.z end, "move up")
+window:set_callback("R", function() p.r = p.r + 0.01 end, "increase red frequency")
+window:set_callback("G", function() p.g = p.g + 0.01 end, "increase green frequency")
+window:set_callback("B", function() p.b = p.b + 0.01 end, "increase blue frequency")
+window:set_callback("r", function() p.r = p.r - 0.01 end, "decrease red frequency")
+window:set_callback("g", function() p.g = p.g - 0.01 end, "decrease green frequency")
+window:set_callback("b", function() p.b = p.b - 0.01 end, "decrease blue frequency")
+window:set_callback("-", function() p.z = p.z * 1.1 end, "zoom out")
+window:set_callback("+", function() p.z = p.z / 1.1 end, "zoom in")
+window:set_callback("idle", function() shade:set_program(vert, frag(p)) end)
 
-while window:render_scene{image} == "continue" do
-   shade:set_program(vert, frag(params))
-end
+while window:render_scene{image} == "continue" do end
